29be943c4cd76924641094b7368635f45e5fd75e
HELOISE GABRIELI JUCHEM <heloise.juchem@escola.pr.gov.br>
2026-05-20 12:35:05 +0000

    ..

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-def main():
-    print("Olá, mundo!")
+# nomes = ["Ana", "Bruno", "Carla", "Diego", "Fernanda"]
+# print(nomes[1])
+# print(len(nomes))
+# nomes.append("Gabriel")
+# nomes.insert(1, "Allan")
+# print(nomes)
+# nomes.clear()
+# print(nomes)
+# del(nomes)
+# print(nomes)
 
-if __name__ == "__main__":
-    main()
+#Exemplo de Dicionario
+info = {
+  "Nomes": ["Ana","Bruno", "Carla"],
+  "Idades": (10, 25, 14),
+  "Parentesco": {
+    "Nome":"Ana",
+    "Pais": ["Maria", "João"]
+  }
+}
+
+# print(info.Nomes)
+
+#Exemplos de Tupla
+notas = (9.4, 8.3, 5.2, 10.0, 4.8)
+print(f"Nota na posição 2: {notas[2]}")
+notas.insert(1, 10.0)
+notas.append(5.8)
+del(notas)
+
